26093ef0e51dea3974030793d5b85b7d30eeb7d0
TABET DJALAL <b_tabet@esi.dz>
2020-11-17 23:31:05 +0100

    EXO 1

---
+++ Exo1/Exo1.py
@@ -0,0 +1,18 @@
+print("Exo 1 Ecrire une fonction qui a en paramètre un tableau non vide et qui renvoie le"
+      "plus petit élément d’un tableau non vide")
+
+tableau=[1,2,4,5];
+
+def plusPetitElementDunTableau(tableau):
+    # if tableau non vide renvoyer le min
+    print("Le plus petit élément du tableau est :")
+    print(min(tableau))
+    # if tableau  vide renvoyer message erreur print"tableau est vide"
+
+
+plusPetitElementDunTableau(tableau)
+
+
+
+
+
