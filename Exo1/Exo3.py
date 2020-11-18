3408d35a0b19934a0084d0c42b6a9253ce4840a0
TABET DJALAL <b_tabet@esi.dz>
2020-11-18 21:14:25 +0100

    exo3

--- Exo1/Exo3.py
+++ Exo1/Exo3.py
@@ -1,6 +1,6 @@
 print("Exo 3) Ecrire une fonction qui a en paramètre un tableau non vide et qui renvoie la"
 "moyenne des éléments d’un tableau")
-
+# https://emilypython.wordpress.com/category/python/
 tableau=[1,2,4,5];
 
 
