ecb895821d5edc049c7daa1d69210d14c52d0fe8
TABET DJALAL <b_tabet@esi.dz>
2020-11-18 19:45:02 +0100

    Exo 1

--- Exo1/Exo1.py
+++ Exo1/Exo1.py
@@ -5,12 +5,16 @@ tableau=[1,2,4,5];
 
 def plusPetitElementDunTableau(tableau):
     # if tableau non vide renvoyer le min
+    mini = tableau[0]
+    for i in tableau:
+        if i <= mini:
+            mini = i
     print("Le plus petit élément du tableau est :")
-    print(min(tableau))
-    # if tableau  vide renvoyer message erreur print"tableau est vide"
-
+    print(mini)
+    return mini
 
 plusPetitElementDunTableau(tableau)
+
 
 
 
